dbc9c654bcd43ff95597331b9c18b13e97906e75
Piotr Zalas <pzalas@onet.eu>
2016-06-06 22:25:56 +0200

    Remote interfaces

---
+++ Interface/IDevice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ServiceModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Interface
+{
+    public interface IDevice
+    {
+        [OperationContract(IsOneWay = true)]
+        void SetImage(Image image);
+
+        [OperationContract(IsOneWay = true)]
+        void SetMenuItems(MenuItem[] items);
+    }
+}
---
+++ Interface/IDriver.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace Interface
+{
+    [ServiceContract(CallbackContract = typeof(IDevice), SessionMode = SessionMode.Required)]
+    public interface IDriver
+    {
+        [OperationContract(IsInitiating = true, IsOneWay = true)]
+        void Authenticate();
+
+        [OperationContract(IsInitiating = false, IsOneWay = true)]
+        void Execute(string action);
+
+        [OperationContract(IsInitiating = false, IsTerminating = true, IsOneWay = true)]
+        void Disconnect();
+    }
+}
