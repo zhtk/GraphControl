c05f319ac55247cb627d6428ac8ef3c2a673dd73
Piotr Zalas <pzalas@onet.eu>
2016-06-06 22:26:46 +0200

    Stub for remote driver 1

---
+++ Driver1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace Driver1
+{
+    class Program
+    {
+        static Server serviceInstance = new Server();
+
+        static void Main()
+        {
+            Console.WriteLine("Driver1 Server");
+            Console.Write("Starting WCF listener...");
+
+            // TODO 3 endpointy zamiast 1
+            using (ServiceHost host = new ServiceHost(serviceInstance))
+            {
+                host.Open();
+                Console.WriteLine(" started.\n");
+                Console.WriteLine("Press [ENTER] to quit.\n");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Driver1/Server.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.ServiceModel;
+
+namespace Driver1
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    class Server : Interface.IDriver
+    {
+        public ConcurrentDictionary<Interface.IDevice, int> devices = new ConcurrentDictionary<Interface.IDevice, int>();
+
+        public void Authenticate()
+        {
+            OperationContext.Current.Channel.Faulted += (sender, args) =>
+                Console.WriteLine("{0} - Client connection failed.", DateTime.Now);
+            OperationContext.Current.Channel.Closed += (sender, args) =>
+            {
+                int unused;
+                devices.TryRemove((Interface.IDevice) sender, out unused);
+                Console.WriteLine("{0} - Client connection closed. {1} active.", 
+                                  DateTime.Now, devices.Count);
+            };
+
+            Interface.IDevice user = OperationContext.Current.GetCallbackChannel<Interface.IDevice>();
+            devices.TryAdd(user, 0);
+            Console.WriteLine("{0} - Client called 'Authenticate'. {1} active.", 
+                              DateTime.Now, devices.Count);
+
+            BroadcastState();
+        }
+
+        public void BroadcastState()
+        {
+            // TODO
+        }
+
+        public void Execute(string action)
+        { 
+            // TODO
+
+            BroadcastState();
+        }
+
+        public void Disconnect()
+        {
+            Console.WriteLine("{0} - Client called 'Disconnect', {1} users active", 
+                              DateTime.Now, devices.Count - 1);
+        }
+    }
+}
